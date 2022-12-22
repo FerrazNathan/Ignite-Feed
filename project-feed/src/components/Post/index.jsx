68df2f073dc92f6d62a51e1ae48237bd84d827d7
Nathan Ferraz <nathanferraz@hotmail.com>
2022-12-22 17:52:26 -0300

    organizing structures

---
+++ project-feed/src/components/Post/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function Post({author, content}) {
+
+  console.log(props, 'props');
+  return (
+    <div>
+      teste
+      {/* <p>{author}</p>
+      <p>{content}</p> */}
+    </div>
+  )
+}

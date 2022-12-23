415b4082d52e57c8ceceb5ce8afd67e492389466
Nathan Ferraz <nathanferraz@hotmail.com>
2022-12-23 10:37:21 -0300

    create a first component Post

---
+++ vite-project/src/pages/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from '../components/Post';
+
+export function App() {
+
+  return (
+    <>
+      <Post
+        author='Nathan Ferraz'
+        content='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi consequuntur ipsam culpa sapiente nisi id minus obcaecati quis dicta beatae suscipit explicabo maxime facere aliquid, repudiandae mollitia cumque quam amet?'
+      />
+    </>
+  )
+}

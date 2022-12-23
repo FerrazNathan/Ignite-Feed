60ccc66bbe8761851c8aa0ca29ef00932b63e60b
Nathan Ferraz <nathanferraz@hotmail.com>
2022-12-23 15:34:01 -0300

    finish Header component

--- vite-project/src/components/Header/index.jsx
+++ vite-project/src/components/Header/index.jsx
@@ -1,9 +1,12 @@
+import IgniteLogo from '../../assets/logo.svg'
 
 import styles from './Header.module.css'
 
 export function Header() {
+
   return (
     <header className={styles.header}>
+      <img src={IgniteLogo} alt="Logotipo" />
       <strong>Ignite Feed</strong>
     </header>
   )
--- vite-project/src/pages/App.jsx
+++ vite-project/src/pages/App.jsx
@@ -7,7 +7,7 @@ export function App() {
 
   return (
     <>
-    <Header />
+      <Header />
       <Post
         author='Nathan Ferraz'
         content='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi consequuntur ipsam culpa sapiente nisi id minus obcaecati quis dicta beatae suscipit explicabo maxime facere aliquid, repudiandae mollitia cumque quam amet?'

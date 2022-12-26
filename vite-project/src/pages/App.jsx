61d02bb5024dd9c903467a8ee1deebfb27fd7ef9
Nathan Ferraz <nathanferraz@hotmail.com>
2022-12-26 12:09:09 -0300

    create a component Sidebar

--- vite-project/src/pages/App.jsx
+++ vite-project/src/pages/App.jsx
@@ -1,6 +1,8 @@
 import { Post } from '../components/Post';
 import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
+import styles from './App.module.css'
 import '../global.css'
 
 export function App() {
@@ -8,10 +10,20 @@ export function App() {
   return (
     <>
       <Header />
-      <Post
-        author='Nathan Ferraz'
-        content='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi consequuntur ipsam culpa sapiente nisi id minus obcaecati quis dicta beatae suscipit explicabo maxime facere aliquid, repudiandae mollitia cumque quam amet?'
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Nathan Ferraz'
+            content='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi consequuntur ipsam culpa sapiente nisi id minus obcaecati quis dicta beatae suscipit explicabo maxime facere aliquid, repudiandae mollitia cumque quam amet?'
+          />
+          <Post
+            author='Nathan Ferraz'
+            content='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi consequuntur ipsam culpa sapiente nisi id minus obcaecati quis dicta beatae suscipit explicabo maxime facere aliquid, repudiandae mollitia cumque quam amet?'
+          />
+        </main>
+
+      </div>
     </>
   )
 }
